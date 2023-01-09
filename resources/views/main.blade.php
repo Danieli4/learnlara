35b56154af7d698586719a39e894ee1d67d01a11
danilgryaznev <daniel58912007@gmail.com>
2023-01-09 21:14:29 +0300

    Laravel курс с нуля, база. 17. CRUD через интерфейс. Имена роутов, контроллеров по конвенции Laravel

---
+++ resources/views/main.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('content')
+    <div>
+        this main page
+    </div>
+@endsection
---
+++ resources/views/post/edit.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.main')
+@section('content')
+    <div>
+        <form action="{{route('post.update', $post->id)}}" method="post">
+            @csrf
+            @method('patch')
+
+            <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" name="title" class="form-control" id="title" value="{{$post->title}}">
+            </div>
+            <div class="mb-3">
+                <label for="content" class="form-label">Content</label>
+                <textarea class="form-control" name="content" id="content" >{{$post->content}}</textarea>
+            </div>
+            <div class="mb-3">
+                <label for="image" class="form-label">Image</label>
+                <input type="text" class="form-control" name="image" id="image" value="{{$post->image}}">
+            </div>
+
+            <button type="submit" class="btn btn-primary" >Update</button>
+            <button type="submit" class="btn btn-primary">Create</button>
+        </form>
+    </div>
+@endsection
