1db6feff7c7b1fd1ff3b2e836d6bddc55f59fab7
danilgryaznev <daniel58912007@gmail.com>
2023-01-09 22:41:10 +0300

    Laravel курс с нуля, база. 18 отношения один ко многим

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
-        //dd($posts);
-        return view('post.index', compact('posts'));
+        $post = Post::find(1);
+        dd($post->category);
+        //return view('post.index', compact('posts'));
     }
 
     public function create()
