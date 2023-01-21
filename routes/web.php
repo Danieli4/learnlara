3e3b37d1ce0b17bd2a87fd985bc78d124544d5e2
danilgryaznev <daniel58912007@gmail.com>
2023-01-21 09:20:26 +0300

    25. Eponymous controllers

--- routes/web.php
+++ routes/web.php
@@ -18,21 +18,30 @@ Route::get('/', function () {
         //view('welcome');
 });
 
-Route::get('/posts', 'App\Http\Controllers\PostController@index')->name('post.index');
-Route::get('/posts/create', 'App\Http\Controllers\PostController@create')->name('post.create');
 
-Route::post('/posts', 'App\Http\Controllers\PostController@store')->name('post.store');
-Route::get('/posts/{post}', 'App\Http\Controllers\PostController@show')->name('post.show');
-Route::get('/posts/{post}/edit', 'App\Http\Controllers\PostController@edit')->name('post.edit');
-Route::patch('/posts/{post}', 'App\Http\Controllers\PostController@update')->name('post.update');
-Route::delete('/posts/{post}', 'App\Http\Controllers\PostController@destroy')->name('post.delete');
+Route::group(['namespace' => 'App\Http\Controllers\Post'], function () {
+    Route::get('/posts', 'IndexController')->name('post.index');
+    Route::get('/posts/create', 'CreateController')->name('post.create');
 
-Route::get('/posts/update', 'App\Http\Controllers\PostController@update');
-Route::get('/posts/delete', 'App\Http\Controllers\PostController@delete');
-Route::get('/posts/restore', 'App\Http\Controllers\PostController@restore');
-Route::get('/posts/first_or_create', 'App\Http\Controllers\PostController@firstOrCreate');
-Route::get('/posts/update_or_create', 'App\Http\Controllers\PostController@updateOrCreate');
+    Route::post('/posts', 'StoreController')->name('post.store');
+    Route::get('/posts/{post}', 'ShowController')->name('post.show');
+    Route::get('/posts/{post}/edit', 'EditController')->name('post.edit');
+    Route::patch('/posts/{post}', 'UpdateController')->name('post.update');
+    Route::delete('/posts/{post}', 'DestroyController')->name('post.delete');
+
+
+
+});
+
+//Route::get('/posts/update', 'App\Http\Controllers\Controller');
+//Route::get('/posts/delete', 'App\Http\Controllers\Controller');
+//Route::get('/posts/restore', 'App\Http\Controllers\Controller');
+//Route::get('/posts/first_or_create', 'App\Http\Controllers\Controller');
+//Route::get('/posts/update_or_create', 'App\Http\Controllers\Controller');
+
 
 Route::get('/main', 'App\Http\Controllers\MainController@index')->name('main.index');
 Route::get('/contacts', 'App\Http\Controllers\ContactController@index')->name('contact.index');
 Route::get('/about', 'App\Http\Controllers\AboutController@index')->name('about.index');
+
+
