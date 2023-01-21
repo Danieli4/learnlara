676dd0fb5c06e9abf24523514641643fb2129d2e
danilgryaznev <daniel58912007@gmail.com>
2023-01-21 22:47:50 +0300

    28. Class Factory and Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use Database\Factories\CategoryFactory;
+use Database\Factories\TagFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +19,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Category::factory(20)->create();
+        $tags =Tag::factory(30)->create();
+        $posts =Post::factory(200)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         foreach ($posts as $post){
+             $tagsId = $tags->random(5)->pluck('id');
+             $post->tags()->attach($tagsId);
+         }
+
+
     }
 }
